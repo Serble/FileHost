b3211438a6942f8ef85b9995307442768896818a
copokbl <zaneharrison2006@icloud.com>
2022-06-30 12:44:52 +1000

    Turned off https redirect

--- Program.cs
+++ Program.cs
@@ -134,8 +134,7 @@ public static class Program {
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
-
-        app.UseHttpsRedirection();
+        
         app.UseStaticFiles();
         app.UseRouting();
         app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
